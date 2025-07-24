e57cac0a89b797a819ea785dfcc506823c49c415
Bora Saglam <posarobit@gmail.com>
2025-07-24 15:15:05 +0300

    Düzenlemeler yapıldı.

--- class_homework.py
+++ class_homework.py
@@ -2,12 +2,12 @@ MIN_DEGER = 0
 MAX_DEGER = 100
 
 class EvcilHayvan:
-    def __init__(self, isim, tur, aclik, uyku, mutluluk, enerji):
+    def __init__(self, isim, tur, aclik, uyku_kontrolu, mutlu_et, enerji):
         self.isim = isim
         self.tur = tur
         self.aclik = aclik
-        self.uyku = uyku
-        self.mutluluk = mutluluk
+        self.uyku_kontrolu = uyku_kontrolu
+        self.mutlu_et = mutlu_et
         self.enerji = enerji
 
     def aclik_kontrol(self):
@@ -28,7 +28,7 @@ class EvcilHayvan:
                 self.enerji = MAX_DEGER
             print(f"{self.isim} ile oynandı.\n")
 
-    def mutluluk(self):
+    def mutluluk_kontrolu(self):
         if self.enerji < 50:
             print(f"{self.isim} az mutlu!")
         else:
@@ -36,14 +36,14 @@ class EvcilHayvan:
             if self.enerji > MAX_DEGER:
                 self.enerji = MAX_DEGER
             print(f"{self.isim} daha mutlu!\n")
-        
+
     def uyku(self):
-        if self.enerji < 50:
+        if self.uyku_kontrolu < 50:
             print(f"{self.isim} uyuması lazım!")
         else:
-            self.enerji -= 30
-            if self.enerji < MIN_DEGER:
-                self.enerji = MIN_DEGER
+            self.uyku_kontrolu -= 30
+            if self.uyku_kontrolu < MIN_DEGER:
+                self.uyku_kontrolu = MIN_DEGER
             self.enerji += 40
             if self.enerji > MAX_DEGER:
                 self.enerji = MAX_DEGER
@@ -58,7 +58,7 @@ class EvcilHayvan:
             print(f"{self.isim} enerjisi yeterli.")
 
     def durum_goster(self):
-        print(f"\n{self.isim} ->> Enerji: {self.enerji}, Açlık: {self.aclik}, Uyku: {self.uyku}, Mutluluk: {self.mutluluk}\n")
+        print(f"\n{self.isim} ->> Enerji: {self.enerji}, Açlık: {self.aclik}, Uyku: {self.uyku_kontrolu}, Mutluluk: {self.mutlu_et}\n")
 
 
 # Kullanıcıdan veri al
@@ -98,7 +98,7 @@ while True:
     elif secim == 5:
         evcil_hayvan.oynat()
     elif secim == 6:
-        evcil_hayvan.mutluluk()
+        evcil_hayvan.mutluluk_kontrolu()
     elif secim == 7:
         print("Çıkış yapılıyor")
         break
